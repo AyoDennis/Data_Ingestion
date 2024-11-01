3fa7f163f366257f7d94a57964f163776baa9a88
AyoDennis <ayodejidennis33@gmail.com>
2024-11-01 17:44:43 +0100

    tested the module

--- extract_pipeline.py
+++ extract_pipeline.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from rename_dataframe import rename_columns
 
 response = requests.get('https://randomuser.me/api/')
 
@@ -8,34 +9,44 @@ results = data['results']
 
 normalized_result = pd.json_normalize(results)
 
-#DATA TRANSFORMATION
-#Convert to dataframe
+# DATA TRANSFORMATION
+# Convert to dataframe
 
 normalized_frame = pd.DataFrame(normalized_result)
 
 
-##DATA TRANSFORMATION: Select Columns
+# DATA TRANSFORMATION: Select Columns
 
-selected_columns = normalized_frame[['name.title', 'name.first', 'name.last', 'cell','email','location.street.number','location.street.name','location.city','location.country',
-                  'login.username', 'login.password', 'dob.date', 'dob.age']]
-
-
-#DATA TRANSFORMATION: Rename Columns
-
-renamed_normalize = selected_columns.rename(columns={
-    'name.title':'title',
-    'name.first':'first_name',
-    'name.last':'last_name',
-    'cell':'phone',
-    'location.street.number':'street_number',
-    'location.street.name':'street_name', 
-    'location.city':'city',
-    'location.country':'country', 
-    'login.username':'username', 
-    'login.password':'password', 
-    'dob.date':'dob',
-    'dob.age':'age'
-})
+selected_columns = normalized_frame[[
+    'name.title',
+    'name.first',
+    'name.last',
+    'cell',
+    'email',
+    'location.street.number',
+    'location.street.name',
+    'location.city',
+    'location.country',
+    'login.username',
+    'login.password',
+    'dob.date',
+    'dob.age'
+    ]]
 
 
+# DATA TRANSFORMATION: Rename Columns
 
+rename_columns(selected_columns, {
+    'name.title': 'title',
+    'name.first': 'first_name',
+    'name.last': 'last_name',
+    'cell': 'phone',
+    'location.street.number': 'street_number',
+    'location.street.name': 'street_name',
+    'location.city': 'city',
+    'location.country': 'country',
+    'login.username': 'username',
+    'login.password': 'password',
+    'dob.date': 'dob',
+    'dob.age': 'age'
+    })
