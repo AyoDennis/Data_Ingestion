fd0058e42da850adfb0bf79ebaf24e1a0f076a9d
AyoDennis <ayodejidennis33@gmail.com>
2024-11-01 16:56:11 +0100

    flake8 changes

--- rename_dataframe.py
+++ rename_dataframe.py
@@ -1,16 +1,20 @@
 def rename_columns(df, new_names):
-  """
-  This function is for renaming columns in a pandas DataFrame.
+    """
+    This function is for renaming columns in a pandas DataFrame.
+    Args:
+        df: The pandas DataFrame to rename columns in.
+        new_names: A dictionary mapping old to new column names in this format.
 
-  Args:
-    df: The pandas DataFrame to rename columns in.
-    new_names: A dictionary mapping old column names to new column names in the format below.
+        new_names =
+        {
+            'old_column_1': 'new_column_1',
+            'old_column_2': 'new_column_2',
+        # ...
+        }
 
-    new_names = 
-        
-  Returns:
-    The DataFrame with renamed columns.
-  """
 
-  df = df.rename(columns=new_names)
-  return df
+    Returns:
+        The DataFrame with renamed columns.
+     """
+    df = df.rename(columns=new_names)
+    return df
