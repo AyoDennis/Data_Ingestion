309ea4c2ec6b8e9e9ede375d676a25178ee9931d
AyoDennis <ayodejidennis33@gmail.com>
2024-11-02 06:30:21 +0100

    redefinition of unused 'normalize_table'

--- extract_pipeline.py
+++ extract_pipeline.py
@@ -5,22 +5,6 @@ from utils import (extract_data, extract_female, extract_male, normalize_table,
                    rename_columns)
 
 
-
-
-def normalize_table(parsed_json):
-    """
-    this takes in the parsed JSON, filters 'results'
-    and returns a normalized dataframe
-    """
-    # filter the index of 'results'
-    results = parsed_json['results']
-    # normalizes everything in the 'results' object
-    normalized_result = pd.json_normalize(results)
-    # converts normalized result into a DataFrame
-    normalized_df = pd.DataFrame(normalized_result)
-    return normalized_df
-
-# helper functions
 url = "https://randomuser.me/api/"
 
 parsed_json = extract_data(url)
