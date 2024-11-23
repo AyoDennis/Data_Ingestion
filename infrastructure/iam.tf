3292fa385df3816fab04cb82b0882bb7795c01d3
AyoDennis <ayodejidennis33@gmail.com>
2024-11-23 22:46:07 +0100

    everything set

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -1,4 +1,3 @@
-# create a dedicated user for airflow instance
 resource "aws_iam_user" "airflow_user" {
   name = "ayodeji_airflow"
 
@@ -8,32 +7,27 @@ resource "aws_iam_user" "airflow_user" {
   }
 }
 
-# Create aws access key and secret key for airflow to programatically interact with AWS
 resource "aws_iam_access_key" "airflow_credentials" {
   user = aws_iam_user.airflow_user.name
 }
 
-# saving the output of the access key into AWS SSM parameters store
 resource "aws_ssm_parameter" "airflow_access_key" {
   name  = "/dev/airflow/ayodeji_airflow_user_access_key"
   type  = "String"
   value = aws_iam_access_key.airflow_credentials.id
 }
 
-# saving the output of the secret key into AWS SSM parameters store
 resource "aws_ssm_parameter" "airflow_secret_key" {
   name  = "/dev/airflow/ayodeji_airflow_user_secret_key"
   type  = "String"
   value = aws_iam_access_key.airflow_credentials.secret
 }
 
-# defining AWS IAM policy which highlight what airflow user can do and cant in AWS
 resource "aws_iam_policy" "airflow_policy" {
   name        = "airflow-policy"
   description = "Dedicated policy for airflow instance "
 
-  # read and write permission 
-  policy = jsonencode({
+    policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
@@ -50,7 +44,6 @@ resource "aws_iam_policy" "airflow_policy" {
   })
 }
 
-# attach the IAM policy to airflow user 
 resource "aws_iam_user_policy_attachment" "test-attach" {
   user       = aws_iam_user.airflow_user.name
   policy_arn = aws_iam_policy.airflow_policy.arn
--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,8 +1,3 @@
-# resource "aws_s3_bucket" "data_ingestion" {
-#   bucket = "ayodeji-data-ingestion-bucket/random_profile"
-# }
-
-
 resource "aws_s3_bucket" "data_ingestion" {
   bucket = "ayodeji-data-ingestion-bucket"
 }
