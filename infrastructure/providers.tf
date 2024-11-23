843d2d68f9d63567ba196edea8641f18e7231664
AyoDennis <ayodejidennis33@gmail.com>
2024-11-23 20:56:48 +0100

    infra

---
+++ infrastructure/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Terraform Remote State
+
+terraform {
+  backend "s3" {
+    bucket = "ayodeji-state-bucket-data-ingestion"
+    key    = "key/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "backend_versioning" {
+  bucket = aws_s3_bucket.ayodeji-state-bucket-data-ingestion.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  }
