8649c2d3b35586d12a5c73428397771028ad6ccb
AyoDennis <ayodejidennis33@gmail.com>
2025-04-09 09:53:22 +0100

    changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 # Terraform Remote State
 terraform {
   backend "s3" {
-    bucket = "ayodeji-state-bucket-data-ingestion"
+    bucket = "ayodeji-state-bucket-data-ingestion-1"
     key    = "key/terraform.tfstate"
     region = "eu-central-1"
   }
